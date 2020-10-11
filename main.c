6d2207b22faccff8a825eb8a331098cf10999308
bengceng <71591780+bengceng@users.noreply.github.com>
2020-10-11 20:02:39 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayici,max,sayi;
+    sayici=1;
+    while(sayici<=10){
+        printf("sayi gir:\n");
+        scanf("%d",&sayi);
+        sayici=sayici+1;
+    if(sayi>max){
+        sayi=max;}
+    }
+  printf("%d en buyuktur.",sayi);
+    return 0;
+}
