5f51bd6e359e955a58032b972444b204eda761fd
bengceng <71591780+bengceng@users.noreply.github.com>
2020-10-11 20:14:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x,y,z;
+    printf("uckenin kenarlarýný girin:");
+    scanf("%f%f%f",&x,&y,&z);
+
+    if(x*x==(y*y)+(z*z)|| y*y==x*x+z*z || z*z==x*x+y*y)
+        printf("dik ucgenin uc kenaridir.");
+
+    else printf("dik ucgenin uc kenari degildir.");
+
+    return 0;
+}
