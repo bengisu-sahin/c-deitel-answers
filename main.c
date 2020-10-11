6bf55286cacca8342fa63295b8bd181a6710f138
bengceng <71591780+bengceng@users.noreply.github.com>
2020-10-11 20:09:25 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a;
+   a=1;
+   while(a<=100){
+    printf("*");
+    if(a%10==0)
+        printf("\n");
+         a=a+1;
+   }
+    return 0;
+}
