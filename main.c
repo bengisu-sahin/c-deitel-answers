43d7dc23a4b7638c7a09cf3d70747fe64d2c0112
bengceng <71591780+bengceng@users.noreply.github.com>
2020-10-11 20:10:01 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,sayac;
+    sayac=0;
+    printf("Bir tamsayi giriniz:\n");
+    scanf("%d",&a);
+    if(a%10==7)
+        sayac++;
+    while(a/7==0){
+        sayac=sayac+1;
+
+    }
+    printf(" %d ",sayac);
+
+    return 0;
+}
