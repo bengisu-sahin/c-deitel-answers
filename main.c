00c90c4b9e04df75c0dbe1c44e634fe86ad86666
bengceng <71591780+bengceng@users.noreply.github.com>
2020-10-11 20:04:37 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A;
+     A=3;
+    printf("A\tA+2\tA+4\tA+6\n");
+
+    while(A<=15){
+      printf("%d\t%d\t%d\t%d\n",A , A+2 , A+4 , A+6);
+      A=A+3;
+    }
+    return 0;
+}
