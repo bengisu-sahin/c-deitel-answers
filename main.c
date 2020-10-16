44e6fd6939496d68817d5e605417b13dda07c66a
bengceng <71591780+bengceng@users.noreply.github.com>
2020-10-16 08:44:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+    for(x=1;x<=10;x++){
+        printf("*\n");
+        for(y=0;y<=9;y++){
+            printf("*");
+        }
+    }
+    return 0;
+}
