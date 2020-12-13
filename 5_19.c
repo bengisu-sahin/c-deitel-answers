fe283eea123ff9ff53fc77c0c489643be595353d
bengceng <71591780+bengceng@users.noreply.github.com>
2020-12-13 15:41:51 +0300

    Add files via upload

---
+++ 5_19.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+void construct(int, int);
+int main() 
+{
+	int side1, side2;
+	printf("Enter rectangular's sides:");
+	scanf_s("%d%d", &side1, &side2);
+	construct(side1, side2);
+
+	system("pause");
+}
+void construct(int side1,int side2) {
+	int x, y;
+	for (x = 1; x <= side1; x++) {
+		for (y = 1; y <= side2; y++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
