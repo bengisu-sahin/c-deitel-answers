fe3894721d539b5b3a871b2d98db720aa7a7e6c5
bengceng <71591780+bengceng@users.noreply.github.com>
2020-12-13 15:38:02 +0300

    Add files via upload

---
+++ 5_15.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+double hypotenuse(double s1,double s2);
+int main() {
+	printf("Sides: %f and %f. Hypotenuse: %f.\n", 3., 4., hypotenuse(3.0, 4.0));
+	printf("Sides: %f and %f. Hypotenuse: %f.\n", 5., 12., hypotenuse(5.0, 12.0));
+	printf("Sides: %f and %f. Hypotenuse: %f.\n", 8., 15., hypotenuse(8.0, 15.0));
+}
+ double hypotenuse(double s1, double s2) {
+	return sqrt (s1* s1 + s2 * s2);
+}
