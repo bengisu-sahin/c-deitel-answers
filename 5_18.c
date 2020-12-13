6c06da3d1f471515ae7ae09368827ab175bdd70e
bengceng <71591780+bengceng@users.noreply.github.com>
2020-12-13 15:40:50 +0300

    Add files via upload

---
+++ 5_18.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+char even(int);
+int main()
+{
+	int a;
+	printf("Enter an integer - Press -1 to exit:");
+	scanf_s("%d", &a);
+	if (a != -1) {
+		printf("%ch", even(a));
+	}
+	char even(int x)
+	{
+		if (x % 2 == 0)
+			return even;
+		else
+			return odd;
+	}
+}
