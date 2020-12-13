42055a1cd4b31de8a567b9bbcf00592dff985ae3
bengceng <71591780+bengceng@users.noreply.github.com>
2020-12-13 15:39:59 +0300

    Add files via upload

---
+++ 5_17_8thedition.c
@@ -0,0 +1,18 @@
+﻿
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int rightTriangle(int, int, int);
+int main()
+{
+	int a, b, c;
+	printf("Enter 3 integer:");
+	scanf_s("%d%d%d", &a, &b, &c);
+	printf("%d", rightTriangle(a, b, c));
+}
+int rightTriangle(int x, int y,int z) {
+	if (x*x + y * y == z * z || y * y + z * z == x * x || x * x + z * z == y * y)
+		return 1;
+	else
+		return 0;
+}
